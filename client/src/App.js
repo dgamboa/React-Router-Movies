756b0b61d39e5404abbed6a06bb3b8ed24c3fd66
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-25 14:39:17 -0600

    Set up routes in App to MovieList and Movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +17,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +34,12 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path={'/'}>
+        <MovieList movies={movieList}/>
+      </Route>
+      <Route path={'/movies/:movieID'}>
+        <Movie />
+      </Route>
     </div>
   );
 }
