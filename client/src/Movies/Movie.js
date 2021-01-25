5b4bbfb12d34b17bbb9351163c0e4bf9cb760f46
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-25 15:36:11 -0600

    Refactor MovieCard to include the JSX used for both MovieList and Movie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
-  const { movies } = props.movies;
 
   const { movieID } = useParams();
 
@@ -34,27 +33,20 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { stars } = movie;
 
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
+        <MovieCard movie={movie} />
         <h3>Actors</h3>
-
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
         ))}
+        <div className="save-button">Save</div>
       </div>
-      <div className="save-button">Save</div>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default function MovieList(props) {
   return (
@@ -14,17 +15,11 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { movie } = props;
 
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+      <MovieCard movie={movie} />
     </div>
   );
 }
